e6edaaa72af95d6c2797627f4a7bdbcd99ef9ceb
Alucard <fire0lord0zuko@proton.me>
2026-02-23 07:22:16 +0200

    Fix logging

--- src/log.h
+++ src/log.h
@@ -2,12 +2,16 @@
 
 #include <stdio.h>
 
-#define LOG(...)                                  \
-    do {                                          \
-        const size_t max_len = 1024;              \
-        char buffer[max_len];                     \
-        snprintf(buffer, max_len, __VA_ARGS__);   \
-        FILE *f = fopen("log.txt", "ab");         \
-        fwrite(buffer, sizeof(char), max_len, f); \
-        fclose(f);                                \
+#define LOG(...)                                                \
+    do {                                                        \
+        const size_t max_len = 1024;                            \
+        char buffer[max_len];                                   \
+        int written = snprintf(buffer, max_len, __VA_ARGS__);   \
+        if (written) {                                          \
+            FILE *f = fopen("log.txt", "ab");                   \
+            if (f) {                                            \
+                fwrite(buffer, sizeof(char), written, f);       \
+                fclose(f);                                      \
+            }                                                   \
+        }                                                       \
     } while (0)
