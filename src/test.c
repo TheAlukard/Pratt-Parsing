be5ac639c4d3a21a36acbac255760004f8bc76c5
Alucard <fire0lord0zuko@proton.me>
2026-03-02 07:42:29 +0200

    Added test function

---
+++ src/test.c
@@ -0,0 +1,19 @@
+#include "test.h"
+#include <time.h>
+#include <stdlib.h>
+
+bool get_random_str(char *buff, size_t len)
+{
+    if (len <= 0 || !buff) return false;
+
+    srand(time(0));
+    size_t i = 0;
+    for (; i < len - 1; i++) {
+        char c = rand() % 128;
+        buff[i] = c;
+    }
+
+    buff[i] = '\0';
+
+    return true;
+}
---
+++ src/test.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stddef.h>
+
+bool get_random_str(char *buff, size_t len);
